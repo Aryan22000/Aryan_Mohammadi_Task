897df3ab9754b06a53951daae46395eeeaa3b230
Aryan22000 <113422176+Aryan22000@users.noreply.github.com>
2023-02-01 10:02:40 +0200

    add the files

---
+++ src/Aryan_Mohammadi_Task/layout/contact.php
@@ -0,0 +1,4 @@
+<?php include "header.php" ?>
+<h1>contact </h1>
+<p>+358 415707884</p>
+<?php include "footer.php" ?>
---
+++ src/Aryan_Mohammadi_Task/layout/ex1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>my first php page</title>
+</head>
+<body>
+    <h1>3.1 a simple PHP script to print your information (Name and your groupid).</h1> 
+
+<?php
+echo "Aryan Mohammadi <br>";
+echo "BBCAP22";
+?>
+<h1>3.2 write PHP code to display the following message.</h1>
+<?php
+echo "Hello world ! My name is \"Aryan\" ";
+?>
+<h1>3.3 Write the PHP code in to display the current date. </h1>
+<?php
+echo date("d.m.Y") . "<br>";;
+?>
+<h1>$title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document. </h1>
+</body>
+</html>
